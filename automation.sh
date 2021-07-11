aa563c3b3e51d7f86cb6490c32b8ed84a06dc6c6
aniruddha-pgdsd <aniruddha.acharya@live.com>
2021-07-11 07:25:55 +0000

    Assignment task 3 is done

--- automation.sh
+++ automation.sh
@@ -18,8 +18,23 @@ fi
 
 myname=aniruddha
 timestamp=$(date '+%d%m%Y-%H%M%S')
+filename=${myname}-httpd-logs-${timestamp}
 s3_bucket=upgrad-aniruddha123
 
-sudo tar -cvf /tmp/${myname}-httpd-logs-${timestamp}.tar /var/log/apache2/*.log
-#sudo gzip -v /tmp/aniruddha-httpd-logs-${timestamp}.tar
-aws s3 cp /tmp/${myname}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
+sudo tar -cvf /tmp/${filename}.tar /var/log/apache2/*.log
+#sudo gzip -v /tmp/$filename.tar
+aws s3 cp /tmp/${filename}.tar s3://${s3_bucket}/${filename}.tar
+
+if [ ! -e /var/www/html/inventory.html ]
+then
+	echo -e "Log Type \tDate Created \t \tType \tSize" > /var/www/html/inventory.html
+fi
+
+size=$(du -sh /tmp/${filename}.tar | cut -f1)
+echo -e "httpd-logs \t$timestamp \ttar \t$size\n" >> /var/www/html/inventory.html
+
+if [ ! -e /etc/cron.d/automation ]
+then
+	sudo echo "30 20 * * * root /root/Automation_Project/automation.sh" > /etc/cron.d/automation
+	sudo chmod 600 /etc/cron.d/automation
+fi
